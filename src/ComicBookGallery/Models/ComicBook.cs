4482423705d82219c5f649c996ae865479454f47
lisaPthomas <lisaparola1128@gmail.com>
2017-05-19 14:48:19 -0700

    Updated ComicBook Model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -8,10 +8,27 @@ namespace ComicBookGallery.Models
     public class ComicBook
     {
         public int Id { get; set; }
-        public string SeriesTitles { get; set; }
+        public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHTML { get; set; } 
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }  
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+        //series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace(" ", "-")
+                     .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
